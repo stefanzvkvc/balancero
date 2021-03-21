de4d2e84ebb71c37c429e6e7b87489dcc8e171f7
Stefan Zivkovic <Stefan.Zivkovic@voiceworks.com>
2021-03-21 13:22:07 +0100

    Uploading project

---
+++ lib/balancero.ex
@@ -0,0 +1,18 @@
+defmodule Balancero do
+  @moduledoc """
+  Documentation for `Balancero`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Balancero.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/balancero_test.exs
@@ -0,0 +1,8 @@
+defmodule BalanceroTest do
+  use ExUnit.Case
+  doctest Balancero
+
+  test "greets the world" do
+    assert Balancero.hello() == :world
+  end
+end
